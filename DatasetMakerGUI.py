70994f9ce119a423189c49626f86b70c05e4c6df
William Wang <iamjerichoholic@hotmail.com>
2020-02-05 12:40:53 +0800

    代码重构

--- DatasetMakerGUI.py
+++ DatasetMakerGUI.py
@@ -9,25 +9,12 @@ from Detector import Transformer
 import argparse
 
 if __name__ == '__main__':
-    lastDraw = None
     parser = argparse.ArgumentParser(description='数据集图像裁剪器')
     parser.add_argument('--save_dir', type=str, help='截取后所保存的文件夹', default='dataset/')
     parser.add_argument('-video', '--video', type=str, help='视频文件名', default=None)
     parser.add_argument('-imgdir', '--image_dir', type=str, help='存放照片的文件夹名', default=None)
     parser.add_argument('-image', '--image', type=str, help='单个图片的文件名', default=None)
     args = parser.parse_args()
-
-    if len(os.listdir(args.save_dir)) is not 0:
-        savedjpgs = os.listdir(args.save_dir)
-        savedjpgs.sort()
-        try:
-            savedIndex = int(savedjpgs[-1].split('.')[0]) + 1
-        except ValueError as e:
-            savedIndex = 1
-    else:
-        savedIndex = 1
-    imageFiles = None
-    imageFilesIndex: int = 0
 
 
 class Sqlite:
@@ -65,36 +52,61 @@ class GUI:
     """
 
     def __init__(self, videoStream):
+        # 初始化属性
         self.videoStream = videoStream
-        self.root = tkinter.Tk()  # 创建主窗口
-        self.root.state("zoomed")
         self.staticText = None
-        self.tkImg = self.frame2TkImage(self.readFrame())
-        # canvas尺寸
-        screenWidth = self.tkImg.width()  # root.winfo_screenwidth()
-        screenHeight = self.tkImg.height()  # root.winfo_screenheight()
-        self.canvas = tkinter.Canvas(self.root, bg='white', width=screenWidth, height=screenHeight)
-        self.canvas.pack()
-        self.canvas.create_image(0, 0, anchor='nw', image=self.tkImg)
-        self.title()
-        self.root.geometry(str(self.tkImg.width()) + 'x' + str(self.tkImg.height()))
-        self.canvas.bind('<Button-1>', self.leftMouse_Down)  # 鼠标左键
-        self.canvas.bind('<B1-Motion>', self.mouse_Drag)  # 鼠标拖动
-        self.canvas.bind('<ButtonRelease-1>', self.leftMouse_Up)  # 鼠标抬起
+        # 创建主窗口
+        self.root = tkinter.Tk()
+        self.root.state("zoomed")
+        self.baseTkImg = self.frame2TkImage(self.readFrame())
+        # 设定主窗口大小，绑定事件
+        self.root.geometry(str(self.baseTkImg.width()) + 'x' + str(self.baseTkImg.height()))
         self.root.bind('<Return>', self.enter_Press)  # enter键
         self.root.bind('<Key>', self.key_Press)  # 数字键
         self.root.bind('<space>', self.space_Press)  # 空格键
+        self.root.resizable(False, False)
+        self.title()  # 刷新界面标题
+        # 刷新canvas，绑定事件
+        screenWidth = self.baseTkImg.width()  # root.winfo_screenwidth()
+        screenHeight = self.baseTkImg.height()  # root.winfo_screenheight()
+        self.canvas = tkinter.Canvas(self.root, bg='white', width=screenWidth, height=screenHeight)
+        self.canvas.pack()
+        self.canvas.create_image(0, 0, anchor='nw', image=self.baseTkImg)
+        self.canvas.bind('<Button-1>', self.leftMouse_Down)  # 鼠标左键
+        self.canvas.bind('<B1-Motion>', self.mouse_Drag)  # 鼠标拖动
+        self.canvas.bind('<ButtonRelease-1>', self.leftMouse_Up)  # 鼠标抬起
         self.canvas.place(x=0, y=0)  # pack(fill=tkinter.Y,expand=tkinter.YES)
-        # self.root.resizable(False, False)
+        # 打开标记记录数据库
+        self.database = Sqlite.OpenConnect()
+        # 初始化鼠标事件的位置容器
         self.X = tkinter.IntVar(value=0)
         self.Y = tkinter.IntVar(value=0)
+        # 初始化一般变量
         self.selectPosition = None
         self.LPCountInPicture = 1
+        global lastDrawedRectangle, imageFiles, imageFilesIndex, saveRawFileName
+        lastDrawedRectangle = None
+        imageFiles = None
+        imageFilesIndex = 0
         self.偏移系数 = 1
-        self.database = Sqlite.OpenConnect()
-        # 启动消息主循环
+        # 分配合理的存储文件名
+        if len(os.listdir(args.save_dir)) is not 0:
+            savedjpgs = os.listdir(args.save_dir)
+            savedjpgs.sort()
+            try:
+                saveRawFileName = int(savedjpgs[-1].split('.')[0]) + 1
+            except ValueError as e:
+                saveRawFileName = 1
+        else:
+            saveRawFileName = 1
+
+    def show(self):
+        """
+        启动消息主循环
+        """
         self.root.mainloop()
 
+    # region 工具函数
     def frame2TkImage(self, frame: np.ndarray) -> ImageTk.PhotoImage:
         """
         转换frame为tkImage
@@ -106,7 +118,7 @@ class GUI:
         """
         return ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
 
-    def videoMode(self) -> bool:
+    def isVideoMode(self) -> bool:
         """
         当前是否为视频模式
         Returns:
@@ -114,7 +126,7 @@ class GUI:
         """
         return self.videoStream is not None
 
-    def imageMode(self) -> bool:
+    def isImageMode(self) -> bool:
         """
         当前是否是图片模式
         Returns:
@@ -122,7 +134,7 @@ class GUI:
         """
         return args.image is not None
 
-    def imagedirMode(self) -> bool:
+    def isImageDirMode(self) -> bool:
         """
         当前是否是图片文件夹模式
         Returns:
@@ -130,19 +142,24 @@ class GUI:
         """
         return args.image_dir is not None
 
+    # endregion
+    # region 读取与显示
+    def setCanvasImg(self, tkImage):
+        self.canvas.create_image(0, 0, anchor='nw', image=tkImage)
+
     def readFrame(self) -> np.ndarray:
         """
         读取一帧，无论何种模式
         Returns:
 
         """
-        if self.videoMode():
+        if self.isVideoMode():
             self.frame = VideoUtil.ReadFrame(self.videoStream)
             return self.frame
-        elif self.imageMode():
+        elif self.isImageMode():
             self.frame = Transformer.Imread(args.image)
             return self.frame
-        elif self.imagedirMode():
+        elif self.isImageDirMode():
             global imageFiles, imageFilesIndex
             if imageFiles is None:
                 imageFiles = os.listdir(args.image_dir)
@@ -154,50 +171,43 @@ class GUI:
 
     def showNextFrame(self, skipTimes=None) -> None:
         '''
-        加载并显示视频之后的图像。
+        加载并显示之后的图像/图片。
         Args:
             skipTimes: 如果是文件夹模式则为跳过几张图片。如果是视频模式则为跳过几秒（偏移系数未变时）。
 
         Returns:
 
         '''
-        # if skippedSeconds is None:
-        #     skippedSeconds = VideoUtil.GetFps(self.videoStream) if self.videoMode() else 1
-        # else:
-        #     skippedSeconds *= VideoUtil.GetFps(self.videoStream) if self.videoMode() else 1
         skipTimes = skipTimes if skipTimes is not None else 1
-        if self.videoMode():
+        if self.isVideoMode():
             VideoUtil.SkipReadFrames(self.videoStream, VideoUtil.GetFps(self.videoStream) * skipTimes * self.偏移系数)
             self.frame = self.readFrame()
         else:
             global imageFilesIndex
             imageFilesIndex += skipTimes - 1  # 标记完索引已自动跳到下一张
-            # self.img = Image.open(r"C:\Users\william\ITCP Web\ScenePics\正常\20200109\20200109091040849.jpg")
-            # self.img = ImageTk.PhotoImage(self.img)
             self.frame = self.readFrame()
 
-        self.tkImg = self.frame2TkImage(self.frame)
-        self.canvas.create_image(0, 0, anchor='nw', image=self.tkImg)
+        self.baseTkImg = self.frame2TkImage(self.frame)
+        self.setCanvasImg(self.baseTkImg)
 
+    # endregion
     def title(self, titleText=None, staticText=None) -> None:
         """
         设置显示窗口标题
         Args:
             titleText: 当前要显示的临时内容
             staticText: 需要一直在标题显示的内容
-
-        Returns:
-
         """
         if staticText is not None:
             self.staticText = staticText
-        mTitle = 'DatasetMaker'
+        mTitle = '智能车牌数据标注器'
         if self.staticText is not None:
             mTitle += ' [' + self.staticText + ']'
         if titleText is not None:
             mTitle += ' (' + titleText + ')'
         self.root.title(mTitle)
 
+    # region 键盘事件
     def key_Press(self, event):
         """
         键盘按下事件
@@ -211,7 +221,7 @@ class GUI:
             self.showNextFrame(int(event.char))
         elif event.char is '0':
             self.showNextFrame(10)
-        elif self.videoMode():
+        elif self.isVideoMode():
             if event.char is '-':
                 self.偏移系数 *= -1
                 self.title(staticText='倍率=' + str(self.偏移系数))
@@ -244,6 +254,8 @@ class GUI:
         """
         self.LPCountInPicture += 1
 
+    # endregion
+    # region 鼠标事件
     # 鼠标左键按下的位置
     def leftMouse_Down(self, event):
         """
@@ -257,7 +269,7 @@ class GUI:
         self.X.set(event.x)
         self.Y.set(event.y)
         # 开始画框的标志
-        self.sel = True
+        self.draging = True
 
     # 鼠标左键移动，显示选取的区域
     def mouse_Drag(self, event):
@@ -269,15 +281,16 @@ class GUI:
         Returns:
 
         """
-        if not self.sel:
+        if not self.draging:
             return
-        global lastDraw
+        global lastDrawedRectangle
         try:
             # 删除刚画完的图形，否则鼠标移动的时候是黑乎乎的一片矩形
-            self.canvas.delete(lastDraw)
+            self.canvas.delete(lastDrawedRectangle)
         except Exception as e:
             pass
-        lastDraw = self.canvas.create_rectangle(self.X.get(), self.Y.get(), event.x, event.y, outline='yellow')
+        lastDrawedRectangle = self.canvas.create_rectangle(self.X.get(), self.Y.get(), event.x, event.y,
+                                                           outline='yellow')
 
     # 获取鼠标左键抬起的位置，记录区域
     def leftMouse_Up(self, event):
@@ -289,45 +302,45 @@ class GUI:
         Returns:
 
         """
-        self.sel = False
+        self.draging = False
         try:
-            self.canvas.delete(lastDraw)
+            self.canvas.delete(lastDrawedRectangle)
         except Exception as e:
             pass
         upx = event.x
         upy = event.y
-        upx = upx
-        upy = upy
         myleft, myright = sorted([self.X.get(), upx])
         mytop, mybottom = sorted([self.Y.get(), upy])
         self.selectPosition = (myleft, myright, mytop, mybottom)
-        print("选择区域：xmin:" + str(self.selectPosition[0]) + ' ymin:' + str(
-            self.selectPosition[2]) + ' xmax:' + str(self.selectPosition[1]) + ' ymax:' + str(
-            self.selectPosition[3]))  # my，对应image中坐标信息
+        print("选择区域：xmin:" + str(myleft) + ' ymin:' + str(mytop) + ' xmax:' + str(myright) + ' ymax:' + str(
+            mybottom))  # 对应image中坐标信息
         self.cutPictureAndShow()
 
+    # endregion
+    # region 裁剪与储存
     def cutPictureAndShow(self) -> None:
         """
         裁剪图片，保存，并显示
         Returns:
 
         """
-        myleft, myright, mytop, mybottom = [x for x in self.selectPosition]
+        left, right, top, bottom = [x for x in self.selectPosition]
         # 新添加Label控件和Entry控件以及Button，接收在canvas中点出的框坐标
-        self.canvas.create_rectangle(myleft, mytop, myright, mybottom, outline="red")
-        global savedIndex
-        savedFile = args.save_dir + str(savedIndex) + '.jpg'
-        GUI.cutImwrite(savedFile, self.frame, self.selectPosition)
-        if self.imagedirMode():
-            self.database.append(imageFiles[imageFilesIndex], savedFile, myleft, myright, mytop, mybottom)
-        elif self.imageMode():
-            self.database.append(args.image, savedFile, myleft, myright, mytop, mybottom)
-        elif self.videoMode():
-            self.database.append('', savedFile, myleft, myright, mytop, mybottom)
-        savedIndex += 1
-        print(savedFile, '已保存')
-        self.title(savedFile + ' 已保存')
-        if self.imageMode():
+        global lastDrawedRectangle
+        lastDrawedRectangle = self.canvas.create_rectangle(left, top, right, bottom, outline="red")
+        global saveRawFileName
+        saveFullFileName = args.save_dir + str(saveRawFileName) + '.jpg'
+        GUI.cutImwrite(saveFullFileName, self.frame, self.selectPosition)
+        if self.isImageDirMode():
+            self.database.append(imageFiles[imageFilesIndex], saveFullFileName, left, right, top, bottom)
+        elif self.isImageMode():
+            self.database.append(args.image, saveFullFileName, left, right, top, bottom)
+        elif self.isVideoMode():
+            self.database.append('', saveFullFileName, left, right, top, bottom)
+        saveRawFileName += 1
+        print(saveFullFileName, '已保存')
+        self.title(saveFullFileName + ' 已保存')
+        if self.isImageMode():
             return
         if self.LPCountInPicture is 1:
             self.showNextFrame()
@@ -349,14 +362,15 @@ class GUI:
         myleft, myright, mytop, mybottom = positions[0]
         clipFrame = image[mytop:mybottom, myleft:myright]
         Transformer.Imwrite(savedFileName, clipFrame)
+    # endregion
 
 
 if __name__ == '__main__':
     if args.video is not None:
         video = args.video
         video = video.replace('\"', '')
-        GUI(VideoUtil.OpenInputVideo(video))
+        GUI(VideoUtil.OpenInputVideo(video)).show()
     elif args.image is not None:
-        GUI(None)
+        GUI(None).show()
     elif args.image_dir is not None:
-        GUI(None)
+        GUI(None).show()
