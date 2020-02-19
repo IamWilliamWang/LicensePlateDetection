6a0a69714df213719914e75de9a7ac0abd922560
William Wang <iamjerichoholic@hotmail.com>
2020-02-19 19:56:28 +0800

    修复copy_files执行逻辑，添加了移动文件的功能

--- copy_files.py
+++ copy_files.py
@@ -3,17 +3,35 @@ import re
 import shutil
 from LPDetector import getInconflictFileName
 
-"""
-基于正则的复制脚本：用于批量复制，命名冲突时可以自动重命名
-"""
-if __name__ == '__main__':
-    fromDirs = input('复制哪个(些)文件夹：').split(';')  # 用;隔开多个文件夹名
+
+def copy_files():
+    """
+    基于正则的复制脚本：用于批量复制，命名冲突时可以自动重命名
+    """
+    fromDirs = input('从哪个/哪些文件夹复制文件：').split(';')  # 用;隔开多个文件夹名
     toDir = input('复制到哪个文件夹：')
-    regex = input('复制哪些文件（使用正则表示）：')
+    regex = input('复制的文件的名称需要满足条件（使用正则表示）：')
     for fromDir in fromDirs:
         selectedFiles = os.listdir(fromDir)
-        for select in selectedFiles:
-            if re.compile(regex).match(select):
-                targetFile, postfix = getInconflictFileName(select)
-                shutil.copy2(os.path.join(fromDir, select), os.path.join(toDir, targetFile))
-                print('已复制 ' + os.path.join(fromDir, select) + ' 到 ' + os.path.join(toDir, targetFile))
+        for sourceFile in selectedFiles:
+            if re.compile(regex).match(sourceFile):
+                targetFile, _ = getInconflictFileName(os.path.join(toDir, sourceFile))
+                shutil.copy2(os.path.join(fromDir, sourceFile), targetFile)
+                print('已复制 ' + os.path.join(fromDir, sourceFile) + ' 到 ' + targetFile)
+
+
+def move_files():
+    fromDirs = input('从哪个/哪些文件夹移动文件：').split(';')  # 用;隔开多个文件夹名
+    toDir = input('移动到哪个文件夹：')
+    regex = input('移动的文件的名称需要满足条件（使用正则表示）：')
+    for fromDir in fromDirs:
+        selectedFiles = os.listdir(fromDir)
+        for sourceFile in selectedFiles:
+            if re.compile(regex).match(sourceFile):
+                targetFile, _ = getInconflictFileName(os.path.join(toDir, sourceFile))
+                os.rename(os.path.join(fromDir, sourceFile), targetFile)
+                print('已移动 ' + os.path.join(fromDir, sourceFile) + ' 到 ' + targetFile)
+
+
+if __name__ == '__main__':
+    move_files()
